e049b33fb5da059b2345e82d6f2d5b5060a64503
QuangPham-usf <quangplm1105@gmail.com>
2025-04-22 10:22:18 -0500

    Chore: Update timezone conversion in fct_review SQL files to use UTC instead of America/Chicago

--- dbt-dags/dags/dbt/ba_transformation/models/marts/fct_review.sql
+++ dbt-dags/dags/dbt/ba_transformation/models/marts/fct_review.sql
@@ -31,8 +31,8 @@ base as (
         value_for_money,
         recommended,
         review as review_text,
-        convert_timezone('America/Los_Angeles', 'America/Chicago', updated_at)::timestamp_ntz as el_updated_at,
-        convert_timezone('America/Los_Angeles', 'America/Chicago', current_timestamp())::timestamp_ntz as t_updated_at
+        convert_timezone('America/Los_Angeles', 'UTC', updated_at)::timestamp_ntz as el_updated_at,
+        convert_timezone('America/Los_Angeles', 'UTC', current_timestamp())::timestamp_ntz as t_updated_at
     from source_data
 ),
 
